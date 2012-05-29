f5e619625418f32faff22ef873b5251779fca354
Ryan Murthick <admin@rymate.co.uk>
2012-05-29 21:23:49 +0100

    Some people call this a small step. I CALLL THIS A GIANT LEAP!

---
+++ src/main/java/ru/tehcode/permissions/bukkit/PermissionsEx.java
@@ -0,0 +1,34 @@
+/*
+* PermissionsEXBridge by rymate1234 (http://ryate.co.uk)
+*
+* THIS PLUGIN IS LICENSED UNDER THE WTFPL - (Do What The Fuck You Want To Public License)
+*
+* This program is free software. It comes without any warranty, to
+* the extent permitted by applicable law. You can redistribute it
+* and/or modify it under the terms of the Do What The Fuck You Want
+* To Public License, Version 2, as published by Sam Hocevar. See
+* http://sam.zoy.org/wtfpl/COPYING for more details.
+*
+* TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+*
+* 0. You just DO WHAT THE FUCK YOU WANT TO.
+*
+*/
+package ru.tehcode.permissions.bukkit;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Ryan
+ */
+public class PermissionsEx extends JavaPlugin {
+    protected static final Logger logger = Logger.getLogger("Minecraft");
+    
+    public void onEnable() {
+        logger.log(Level.INFO, "[PermissionsEXBridge] v" + this.getDescription().getVersion() + " enabled");
+    }
+}
