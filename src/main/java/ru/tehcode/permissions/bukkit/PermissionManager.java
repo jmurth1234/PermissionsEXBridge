407962f7762bcf1aeed90b5c1ddb44df8f0a0a23
Ryan Murthick <admin@rymate.co.uk>
2012-05-30 22:10:16 +0100

    BASIC FUNCTIONS COMPLETE!!!

--- src/main/java/ru/tehcode/permissions/bukkit/PermissionManager.java
+++ src/main/java/ru/tehcode/permissions/bukkit/PermissionManager.java
@@ -14,18 +14,23 @@ import org.bukkit.entity.Player;
 public class PermissionManager {
 
     public PermissionUser getUser(String name) {
-        return(getUser(Bukkit.getPlayer(name)));
+        //first, null checks.
+        if (name == null || name.isEmpty()) {
+            return null;
+        }
+        //if their name ain't null, go get 'em tiger! :D
+        return (getUser(Bukkit.getPlayer(name)));
     }
-    
+
     public PermissionUser getUser(Player player) {
         return new PermissionUser(player);
-    }  
+    }
 
     public boolean has(Player player, String permission) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return player.hasPermission(permission);
     }
 
     public boolean has(Player player, String permission, String world) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return player.hasPermission(permission);
     }
 }
